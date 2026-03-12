112585311c19393b5668cf4301001276894b1064
Md Istekhar <124462873+mdistekhar@users.noreply.github.com>
2026-03-13 01:23:23 +0530

    Update clone.groovy

--- clone.groovy
+++ clone.groovy
@@ -1,5 +1,5 @@
 def call(String url , String) {
       echo 'Cloning the Code...'
-                git branch: 'main',
-                url: 'https://github.com/mdistekhar/Blinkit-Clone.git'
+                git url: url ,  branch: branch
+                echo 'Code cloning Successfully....'
 }
